c13aaae6a5ee434b47f22a2587256cbeeb7f8da1
themerous <lhs981130@gmail.com>
2024-06-07 16:18:17 +0900

    feat: minor code update on index.js & created new element views, index.html

--- Practice_Project_1/index.js
+++ Practice_Project_1/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.write("Hello World");
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`express server opened at port : ${port}`);
+});
