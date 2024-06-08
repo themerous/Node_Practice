f9860adc2ddb7f8f0de2c205564b859cc126a753
themerous <lhs981130@gmail.com>
2024-06-09 00:01:25 +0900

    feat: testing the res.write

--- Practice_Project_1/index.js
+++ Practice_Project_1/index.js
@@ -5,6 +5,7 @@ const port = 8080;
 
 app.get('/', (req, res) => {
     res.write("Hello World");
+    res.write("<br>this is a test writing to see if it is displayed on the next line");
     res.end();
 });
 
