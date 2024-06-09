d97ea834a056a68c169b2a0ce149e04ea634b8bf
themerous <lhs981130@gmail.com>
2024-06-09 22:05:49 +0900

    feat: set '/' to lead to views/index.html

--- Practice_Project_1/index.js
+++ Practice_Project_1/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8080;
 
+/* "/"로 처음 접근시 해당 내용 실행한다 */
 app.get('/', (req, res) => {
-    res.write("Hello World");
-    res.write("<br>this is a test writing to see if it is displayed on the next line");
-    res.end();
+    res.sendFile("/views/index.html", {root : __dirname});
+});
+
+app.get('/login', (req, res) => {
+    req.setEncoding('utf-8');
 });
 
 app.listen(port, () => {
