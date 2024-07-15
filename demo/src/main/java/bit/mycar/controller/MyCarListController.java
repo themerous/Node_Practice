06f65a74fd0424980e0d644db42f6c75781ee33e
themerous <lhs981130@gmail.com>
2024-07-15 20:39:50 +0900

    feat: work done for day 0715

--- demo/src/main/java/bit/mycar/controller/MyCarListController.java
+++ demo/src/main/java/bit/mycar/controller/MyCarListController.java
@@ -2,13 +2,23 @@ package bit.mycar.controller;
 
 import java.util.List;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import bit.mycar.data.MycarDto;
 import bit.mycar.service.MyCarService;
 import lombok.RequiredArgsConstructor;
 import naver.storage.NcpObjectStorageService;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +29,36 @@ public class MyCarListController {
     private String bucket = "jpa-practice";
     private String folder = "test-bucket";
 
+    @PostMapping("/mycar")
+    public void uploadCar(
+            @RequestBody MycarDto dto) {
+        System.out.println("insert dto >> " + dto);
+        myCarService.uploadCar(dto);
+    }
+
+    @PutMapping("/mycar/{id}")
+    public void putMethodName(
+            @PathVariable Long id,
+            @RequestBody MycarDto dto) {
+        myCarService.updateCar(id, dto);
+    }
+
+    @DeleteMapping("/mycar/{num}")
+    public void deleteCar(@PathVariable Long num) {
+        myCarService.deleteCar(num);
+    }
+
     @GetMapping("/mycar/list")
     public List<MycarDto> list() {
         return myCarService.getAllCars();
+    }
+
+    @PostMapping("/mycar/photo")
+    public Map<String, String> uploadPhoto(
+            @RequestParam("upload") MultipartFile upload) {
+        String carPhoto = storageService.uploadFile(bucket, folder, upload);
+        Map<String, String> map = new HashMap<>();
+        map.put("carphoto", carPhoto);
+        return map;
     }
 }
--- demo/src/main/java/bit/mycar/service/MyCarService.java
+++ demo/src/main/java/bit/mycar/service/MyCarService.java
@@ -16,4 +16,16 @@ public class MyCarService {
     public List<MycarDto> getAllCars() {
         return dao.getAllDatas();
     }
+
+    public void uploadCar(MycarDto dto) {
+        dao.uploadCar(dto);
+    }
+
+    public void updateCar(Long id, MycarDto dto) {
+        dao.updateCar(id, dto);
+    }
+
+    public void deleteCar(Long num) {
+        dao.deleteCar(num);
+    }
 }
